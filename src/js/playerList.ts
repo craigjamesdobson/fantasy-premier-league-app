0590903cbd03a4eda3a211f9fbc49bc4be4111e6
Craig <dobson.cj@gmail.com>
2018-03-15 22:19:57 +0000

    create new arrays for columns

--- src/js/playerList.ts
+++ src/js/playerList.ts
@@ -17,16 +17,26 @@ namespace PlayerList {
         
         PlayerData.getPlayerData(function (playerData: any) {
 
+            console.log(Math.ceil(playerData.goalkeepers.length / 2));
+
+            var dividedPlayerData = {
+                gkLeft: playerData.goalkeepers.slice(0, Math.floor(playerData.goalkeepers.length / 2)),
+                gkRight: playerData.goalkeepers.slice(Math.floor(playerData.goalkeepers.length / 2)),
+                dfLeft: playerData.defenders.slice(0, Math.floor(playerData.defenders.length / 2)),
+                dfRight: playerData.defenders.slice(Math.floor(playerData.defenders.length / 2)),
+            };
+
+            
+            console.log(dividedPlayerData);
+
             // Retrieve the template data from the HTML (jQuery is used here).
-            var template = $('#handlebars-demo').html();
+            var template = $('#players-template').html();
 
             // Compile the template data into a function
             var templateScript = Handlebars.compile(template);
 
-            //var context = PlayerData.getPlayerData();
-
             // html = 'My name is Ritesh Kumar. I am a developer.'
-            var html = templateScript(playerData);
+            var html = templateScript(dividedPlayerData);
 
             // Insert the HTML code into the page
             $('.player-container').append(html);
