65f727e36bccf49bcf91b3b5638705edb107fd88
Craig Dobson <craig@webselect.net>
2018-04-30 16:56:29 +0100

    create fantasy teams array (WIP)

--- src/js/components/CreatedTeams/CreateTeams.ts
+++ src/js/components/CreatedTeams/CreateTeams.ts
@@ -7,6 +7,12 @@ import { PlayerPosition } from "../../components/Players/PlayerPosition";
 import FantasyTeams from "../CreatedTeams/FantasyTeams.json";
 
 const fantasyTeams = (FantasyTeams as any).fantasy_teams;
+let newFantasyTeam: any = [
+    {
+        team_players: [],
+    },
+];
+const newFantasyTeams: any = [];
 
 export namespace CreateTeams {
     // Interface for Team elements
@@ -35,27 +41,54 @@ export namespace CreateTeams {
 
     export function CreateTeam() {
         PlayerData.getPlayerData((playerData: any) => {
-            // tslint:disable-next-line
-            console.log(playerData);
 
             for (const team of fantasyTeams) {
+
+                newFantasyTeam.unshift(
+                    {
+                        team_id: team.team_id,
+                        team_name: team.team_name,
+                    }
+                );
+
                 $(".teams-container").append(`<h4>${team.team_name}</h4>`);
 
                 for (const teamPlayer of team.team_players) {
-
                     for (const player of playerData.players) {
                         if (player.id === teamPlayer.player_id) {
 
+                            newFantasyTeam[1].team_players.push(
+                                {
+                                    player_id: player.id,
+                                    player_name: player.name,
+                                    player_teamshort: player.teamShort,
+                                    player_type: player.playerType,
+                                }
+                            );
+
                             // tslint:disable-next-line
-                            console.log(player);
-                            $(".teams-container").append(`<p>${player.name} ${player.id} ${player.teamShort} ${player.playerType}</p>`);
+                            //console.log(player);
+
+                            $(".teams-container").append(
+                                `<p>${player.name} ${player.id} ${
+                                    player.teamShort
+                                } ${player.playerType}</p>`
+                            );
                         }
                     }
                 }
-            }
-        });
 
-        // tslint:disable-next-line
-        console.log(fantasyTeams);
+                newFantasyTeams.push(newFantasyTeam);
+                newFantasyTeam = [
+                    {
+                        team_players: [],
+                    },
+                ];
+
+            }
+
+            // tslint:disable-next-line
+            console.log(newFantasyTeams);
+        });
     }
 }
