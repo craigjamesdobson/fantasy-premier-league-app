fc05e73f9c5d78857b16ed573d429f837e11f71a
Craig <craig@webselect.net>
2019-07-03 15:03:03 +0100

    set team value allowed depending on if they are using transfers

--- src/js/components/DraftedTeams/CompleteDraftedTeam.ts
+++ src/js/components/DraftedTeams/CompleteDraftedTeam.ts
@@ -7,6 +7,7 @@ export class CompleteDraftedTeam {
   public readonly teamName: string;
   private readonly isInvalidTeam: boolean;
   private readonly allowedTransfers: boolean;
+  private readonly teamValueAllowed: number;
   private readonly teamPlayers: CompleteDraftedPlayer[];
 
   constructor(draftedTeam: DraftedTeam, players: ICompleteDraftedPlayer[]) {
@@ -14,13 +15,14 @@ export class CompleteDraftedTeam {
     this.teamName = draftedTeam.teamName;
     this.isInvalidTeam = false;
     this.allowedTransfers = draftedTeam.allowedTransfers;
+    this.teamValueAllowed = this.allowedTransfers ? 80 : 90;
     this.teamPlayers = players.map(
       x => new CompleteDraftedPlayer(x.player, x.transfers)
     );
 
     const teamCostTotal = this.teamPlayers.reduce((accumulator, current) => accumulator += parseInt(current.playerPrice, 10), 0);
 
-    if (teamCostTotal > 80) {
+    if (teamCostTotal > this.teamValueAllowed) {
       this.isInvalidTeam = true;
     }
   }
