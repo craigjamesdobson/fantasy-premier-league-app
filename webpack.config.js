558abb8e3c5b6a45520bd04e3070e8ac66dae5b0
Craig Dobson <dobson.cj@gmail.com>
2020-08-19 11:23:46 +0100

    build commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,174 +1,169 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const terserPlugin = require('terser-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
-const zlib = require('zlib');
+const terserPlugin = require("terser-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const devMode = process.env.NODE_ENV !== 'production';
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
   entry: {
-    Index: './src/js/Index.ts',
-    Players: './src/js/views/Players/Players.ts',
-    Teams: './src/js/views/Teams/Teams.ts',
-    Calculations: './src/js/views/Calculations/Calculations.ts',
-    Table: './src/js/views/Table/Table.ts'
+    Index: "./src/js/Index.ts",
+    Players: "./src/js/views/Players/Players.ts",
+    Teams: "./src/js/views/Teams/Teams.ts",
+    Calculations: "./src/js/views/Calculations/Calculations.ts",
+    Table: "./src/js/views/Table/Table.ts",
   },
   output: {
-    path: __dirname + '/docs',
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js'
+    path: __dirname + "/docs",
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].bundle.js",
   },
   devServer: {
-    contentBase: path.join(__dirname, 'src/img'),
+    contentBase: path.join(__dirname, "src/img"),
     compress: false,
     port: 8080,
-    open: true
+    open: true,
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        loader: 'babel-loader',
-        exclude: [/node_modules/]
+        loader: "babel-loader",
+        exclude: [/node_modules/],
       },
       {
         test: /\.scss$/,
         use: [
-          devMode ? MiniCssExtractPlugin.loader : 'style-loader',
+          devMode ? MiniCssExtractPlugin.loader : "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               sourceMap: true,
-              importLoader: 2
-            }
+              importLoader: 2,
+            },
           },
-          'postcss-loader',
-          'sass-loader'
-        ]
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.(png|svg|jpg|gif|pdf)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: '/'
-            }
-          }
-        ]
+              name: "[name].[ext]",
+              outputPath: "/",
+            },
+          },
+        ],
       },
       {
         test: /\.hbs$/,
-        loader: 'handlebars-loader',
+        loader: "handlebars-loader",
         options: {
-          helperDirs: path.join(__dirname, 'src/js/components/Helpers'),
+          helperDirs: path.join(__dirname, "src/js/components/Helpers"),
           precompileOptions: {
-            knownHelpersOnly: false
-          }
-        }
-      }
-    ]
+            knownHelpersOnly: false,
+          },
+        },
+      },
+    ],
   },
   plugins: [
-    new CleanWebpackPlugin([__dirname + '/docs']),
+    new CleanWebpackPlugin([__dirname + "/docs"]),
     // This automatically adds aliases to the application scope for the specified packages
     // So packages which look for the 'jQuery' global alias still work within our app closure
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
+      $: "jquery",
+      jQuery: "jquery",
     }),
 
-    new CopyWebpackPlugin([{ from: 'src/data/', to: '' }]),
-    new CopyWebpackPlugin([{ from: 'src/misc/', to: '' }]),
+    new CopyWebpackPlugin([{ from: "src/data/", to: "" }]),
+    new CopyWebpackPlugin([{ from: "src/misc/", to: "" }]),
 
     // Generates default index.html
     new HtmlWebpackPlugin({
-      title: 'Fantasy Premier League App',
-      chunks: ['Index'],
-      filename: 'index.html',
-      template: 'views/index.html'
+      title: "Fantasy Premier League App",
+      chunks: ["Index"],
+      filename: "index.html",
+      template: "views/index.html",
     }),
 
     // Generates players.html
     new HtmlWebpackPlugin({
-      title: 'Fantasy Premier League App - Players',
-      chunks: ['Index', 'Players'],
-      filename: 'players.html',
-      template: 'views/players.html'
+      title: "Fantasy Premier League App - Players",
+      chunks: ["Index", "Players"],
+      filename: "players.html",
+      template: "views/players.html",
     }),
 
     // Generates teams.html
     new HtmlWebpackPlugin({
-      title: 'Fantasy Premier League App - Teams',
-      chunks: ['Index', 'Teams'],
-      filename: 'teams.html',
-      template: 'views/teams.html'
+      title: "Fantasy Premier League App - Teams",
+      chunks: ["Index", "Teams"],
+      filename: "teams.html",
+      template: "views/teams.html",
     }),
 
     // Generates calculator.html
     new HtmlWebpackPlugin({
-      title: 'Fantasy Premier League App - Calculator',
-      chunks: ['Index', 'Calculations'],
-      filename: 'calculator.html',
-      template: 'views/calculator.html'
+      title: "Fantasy Premier League App - Calculator",
+      chunks: ["Index", "Calculations"],
+      filename: "calculator.html",
+      template: "views/calculator.html",
     }),
 
     // Generates table.html
     new HtmlWebpackPlugin({
-      title: 'Fantasy Premier League App - Table',
-      chunks: ['Index', 'Calculations', 'Table'],
-      filename: 'table.html',
-      template: 'views/table.html'
+      title: "Fantasy Premier League App - Table",
+      chunks: ["Index", "Calculations", "Table"],
+      filename: "table.html",
+      template: "views/table.html",
     }),
 
     // Generates table.html
     new HtmlWebpackPlugin({
-      title: 'Fantasy Premier League App - Rules',
-      chunks: ['Index'],
-      filename: 'rules.html',
-      template: 'views/rules.html'
+      title: "Fantasy Premier League App - Rules",
+      chunks: ["Index"],
+      filename: "rules.html",
+      template: "views/rules.html",
     }),
-
     // Extract css into seperate files
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[name].css'
+      filename: "[name].css",
+      chunkFilename: "[name].css",
     }),
 
-    new CompressionPlugin({
-      filename: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.(js|css|html|svg)$/,
-      threshold: 10240,
-      minRatio: 0.8,
-      deleteOriginalAssets: true,
-    }),
+    new BundleAnalyzerPlugin(),
   ],
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: [".tsx", ".ts", ".js"],
   },
   optimization: {
+    splitChunks: { chunks: "all" },
     minimizer: [
       new terserPlugin({
         cache: true,
         parallel: true,
-        sourceMap: true // set to true if you want JS source maps
-      })
-    ]
+        sourceMap: true, // set to true if you want JS source maps
+      }),
+    ],
   },
   externals: {
-    jquery: 'jQuery',
-    Handlebars: 'Handlebars'
-  }
+    jquery: "jQuery",
+    Handlebars: "Handlebars",
+  },
 };
