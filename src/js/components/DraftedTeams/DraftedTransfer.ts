0baeab2ff71e1cf3b4b31e2074966a23737fb642
Craig Dobson <dobson.cj@gmail.com>
2021-02-19 20:01:05 +0000

    add custom date data that will show active transfer until current date passes it

--- src/js/components/DraftedTeams/DraftedTransfer.ts
+++ src/js/components/DraftedTeams/DraftedTransfer.ts
@@ -3,9 +3,11 @@ import { IDraftedTransferData } from '../DraftedTeams/IDraftedTransferData';
 export class DraftedTransfer {
   public readonly transferId: number;
   public readonly transferWeek: number;
+  public readonly currentTransferDateExpiry: string;
 
   constructor(draftedTransfer: IDraftedTransferData) {
     this.transferId = draftedTransfer.transfer_id;
     this.transferWeek = draftedTransfer.transfer_week;
+    this.currentTransferDateExpiry = draftedTransfer.currentTransferDateExpiry;
   }
 }
